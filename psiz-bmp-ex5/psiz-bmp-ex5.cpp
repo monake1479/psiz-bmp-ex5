78d3085fed36df18d63c04878bdd61dfdebd4a68
Daniel <daniel.oblak99@gmail.com>
2022-05-31 18:56:31 +0200

    Implemented basic information about .bmp file

--- psiz-bmp-ex5/psiz-bmp-ex5.cpp
+++ psiz-bmp-ex5/psiz-bmp-ex5.cpp
@@ -30,7 +30,36 @@ struct ColourRGB {
 
 int main(int arc, char * argv[])
 {
-	printf("Hello world!");
+    FILE* f = fopen(argv[1], "rb");
+
+    if (f == nullptr)
+    {
+        printf("\n\n Can't open the file");
+        return -1;
+    }
+    else
+    {
+        printf("\n\n File f opened!");
+    }
+
+    printf("\n INFORMACJE O BITMAPIE\n\n");
+
+    fread(&File.bfType, sizeof(File.bfType), 1, f);
+    printf(" Typ: %x", File.bfType);
+
+    fread(&File.bfSize, sizeof(File.bfSize), 1, f);
+    printf("\n Rozmiar pliku: %d bajtow", File.bfSize);
+
+    fread(&File.bfReserved1, sizeof(File.bfReserved1), 1, f);
+    printf("\n Zarezerwowane 1: %d", File.bfReserved1);
+
+    fread(&File.bfReserved2, sizeof(File.bfReserved2), 1, f);
+    printf("\n Zarezerwowane 2: %d", File.bfReserved2);
+
+    fread(&File.bfOffBits, sizeof(File.bfOffBits), 1, f);
+    printf("\n Pozycja danych obrazkowych: %d", File.bfOffBits);
+
+    printf("\n");
 }
 
 
