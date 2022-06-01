897c3dcb0c6bb489e5cc25d1d5146477fdff028d
Daniel <daniel.oblak99@gmail.com>
2022-06-01 15:35:23 +0200

    added resolution calculation x,y, colours calculation and bites per pixel

--- psiz-bmp-ex5/psiz-bmp-ex5.cpp
+++ psiz-bmp-ex5/psiz-bmp-ex5.cpp
@@ -74,7 +74,26 @@ int main(int arc, char * argv[])
     fread(&Picture.biPlanes, sizeof(Picture.biPlanes), 1, f);
     printf("\n Liczba platow: %d", Picture.biPlanes);
 
-    
+    fread(&Picture.biBitCount, sizeof(Picture.biBitCount), 1, f);
+    printf("\n Liczba bitow na piksel: %d (1, 4, 8, or 24)", Picture.biBitCount);
+
+    fread(&Picture.biCompression, sizeof(Picture.biCompression), 1, f);
+    printf("\n Kompresja: %d (0=none, 1=RLE-8, 2=RLE-4)", Picture.biCompression);
+
+    fread(&Picture.biSizeImage, sizeof(Picture.biSizeImage), 1, f);
+    printf("\n Rozmiar samego rysunku: %d", Picture.biSizeImage);
+
+    fread(&Picture.biXPelsPerMeter, sizeof(Picture.biXPelsPerMeter), 1, f);
+    printf("\n Rozdzielczosc pozioma: %d", Picture.biXPelsPerMeter);
+
+    fread(&Picture.biYPelsPerMeter, sizeof(Picture.biYPelsPerMeter), 1, f);
+    printf("\n Rozdzielczosc pionowa: %d", Picture.biYPelsPerMeter);
+
+    fread(&Picture.biClrUsed, sizeof(Picture.biClrUsed), 1, f);
+    printf("\n Liczba kolorow w palecie: %d", Picture.biClrUsed);
+
+    fread(&Picture.biClrImportant, sizeof(Picture.biClrImportant), 1, f);
+    printf("\n Wazne kolory w palecie: %d", Picture.biClrImportant);
 }
 
 
