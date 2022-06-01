7e15daca7ad789ed512eef577e7579b335f1852e
Daniel <daniel.oblak99@gmail.com>
2022-06-01 15:39:41 +0200

    impelementation negative file creation

--- psiz-bmp-ex5/psiz-bmp-ex5.cpp
+++ psiz-bmp-ex5/psiz-bmp-ex5.cpp
@@ -94,6 +94,57 @@ int main(int arc, char * argv[])
 
     fread(&Picture.biClrImportant, sizeof(Picture.biClrImportant), 1, f);
     printf("\n Wazne kolory w palecie: %d", Picture.biClrImportant);
+
+
+
+    FILE* negative = fopen("negative.bmp", "wb");
+    if (negative == nullptr)
+    {
+        printf("\n\n Can't open the file");
+        return -1;
+    }
+    else
+    {
+        printf("\n\n File negative opened!");
+    }
+
+    fseek(negative, 0, SEEK_SET);
+    fwrite(&File.bfType, sizeof(File.bfType), 1, negative);
+    fwrite(&File.bfSize, sizeof(File.bfSize), 1, negative);
+    fwrite(&File.bfReserved1, sizeof(File.bfReserved1), 1, negative);
+    fwrite(&File.bfReserved2, sizeof(File.bfReserved2), 1, negative);
+    fwrite(&File.bfOffBits, sizeof(File.bfOffBits), 1, negative);
+
+    fseek(negative, 14, SEEK_SET);
+    fwrite(&Picture.biSize, sizeof(Picture.biSize), 1, negative);
+    fwrite(&Picture.biWidth, sizeof(Picture.biWidth), 1, negative);
+    fwrite(&Picture.biHeight, sizeof(Picture.biHeight), 1, negative);
+    fwrite(&Picture.biPlanes, sizeof(Picture.biPlanes), 1, negative);
+    fwrite(&Picture.biBitCount, sizeof(Picture.biBitCount), 1, negative);
+    fwrite(&Picture.biCompression, sizeof(Picture.biCompression), 1, negative);
+    fwrite(&Picture.biSizeImage, sizeof(Picture.biSizeImage), 1, negative);
+    fwrite(&Picture.biXPelsPerMeter, sizeof(Picture.biXPelsPerMeter), 1, negative);
+    fwrite(&Picture.biYPelsPerMeter, sizeof(Picture.biYPelsPerMeter), 1, negative);
+    fwrite(&Picture.biClrUsed, sizeof(Picture.biClrUsed), 1, negative);
+    fwrite(&Picture.biClrImportant, sizeof(Picture.biClrImportant), 1, negative);
+
+    fseek(negative, sizeof(File.bfOffBits), SEEK_SET);
+
+    int bmpImg;
+    for (int i = File.bfOffBits; i < File.bfSize; i++)
+    {
+        fseek(f, i, SEEK_SET);
+        fseek(negative, i, SEEK_SET);
+        fread(&bmpImg, 3, 1, f);
+        bmpImg = INT_MAX - bmpImg; 
+        fwrite(&bmpImg, 3, 1, negative);
+    }
+
+    printf("\n");
+    fclose(f);
+    fclose(negative);
+
+    return 0;
 }
 
 
